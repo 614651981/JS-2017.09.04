7083a3196350d3b41fd5a4464ca307e5e731cd9a
Your Name <614651981@qq.com>
2017-09-11 23:05:39 +0800

    'JS-2017.09.04'

---
+++ js/index.js
@@ -0,0 +1,127 @@
+// alert(3);
+// alert('a');
+// var a=3;
+// var b=55;
+// console.log(a+b);
+
+// var c;
+// c=21;
+// alert(c);
+// var c=22;
+// c=23;
+// alert(c);
+
+// var d=15;
+// alert(d);
+// e=20;
+// alert(e);
+
+// var num1 = 43;
+// var num2 = 40;
+// var info = '我们班由两个班组成，第一个班505有'+num1+'人, 第二个班507有'+num2+"人,总共有"+(num1+num2)+"人。"
+// var info = `我们班由两个班组成，第一个班505有${num1}人, 第二个班507有${num2}人,总共有${num1+num2}人。`
+// document.write(info);
+// console.log(info);
+// var str1=`
+// 123
+// 456
+// 789`;
+// console.log(str1);
+// alert(Number.MAX_VALUE);
+
+// alert(false==0);
+// alert(false===0);
+// var arr=[1,3,5,7,9.5];
+// arr.length=3;
+// arr.length=9;
+// arr.reverse();
+// alert(arr);
+// var arr1=[1,2,3,4,5,6,7,8];
+// arr1.splice(3, 3, 'me', 'you');
+// alert(arr1);
+// var a=10;
+// if(a>=11){
+// 	alert(true);
+// }else{
+// 	alert(false);
+// }
+// var score=prompt("请输入你的成绩");
+// if(score>=90 && score<=100){
+// 	alert("优秀");
+// }else if(score>=80 && score<90){
+// 	alert("良好");
+// }else if(score>=60 && score<80){
+// 	alert("及格");
+// }else if(score>=0 && score<60){
+// 	alert("不及格");
+// }else{
+// 	alert("输入错误！");
+// }
+// var num=10; num1=20;
+// console.log(num<num1 && num==10);
+// console.log(num<num1 && num);
+// console.log(num<num1 && 10);
+// console.log(num>num1 && num<9);
+// console.log(num && num<9);
+// var week=prompt('请输入星期',1);
+// switch(week){
+// 	case '1':
+// 	   alert('第一组');
+// 	break; 
+// 	case '2':
+// 	   alert('第二组');
+// 	break;
+// 	case '3':
+// 	   alert('第三组');
+// 	break;
+// 	default:
+// 	   alert('输入有误哦*-*')
+// }
+// var week=prompt('请输入星期',1)/1;
+// switch(week){
+// 	case 1:
+// 	   alert('第一组');
+// 	break; 
+// 	case 2:
+// 	   alert('第二组');
+// 	break;
+// 	case 3:
+// 	   alert('第三组');
+// 	break;
+// 	default:
+// 	   alert('输入有误哦*-*')
+// }
+// alert(undefined==null);
+
+// for(var i=1; i<=10; i++){
+// 	alert(1);
+// }
+
+// var sum=0;
+// for(var a=1 ;a<11;a++){
+// 	if(a%2==0){
+// 		alert(sum+=a);
+// 	}
+// }
+// for(var a=1 ;a<11;a++){
+// 	if(a%2!=0){
+// 		sum+=a;
+// 	}
+// }
+// alert(sum);
+// var a=10,b=20;
+// alert( b=0 || a>b);
+// num+=10  num=num+10 
+
+
+
+
+// num=+10
+// num=-10
+// num=a*(b*c)
+var a='11px', b=12;
+// console.log(a)
+// alert(a>b)
+NaN===NaN
+isNaN(NaN);
+console.log(isNaN())
